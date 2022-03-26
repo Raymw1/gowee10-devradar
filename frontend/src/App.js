3c5a0e13865927ffd36a5d5fd6d49bd725b8d9e0
Raymw1 <rayan.rmw@gmail.com>
2022-03-25 21:52:22 -0300

    Frontend done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,122 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
 import "./Main.css";
+import DevItem from "./components/DevItem";
+import DevForm from "./components/DevForm";
 
 export default function App() {
+  const [devs, setDevs] = useState([]);
+
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log(position);
-      },
-      (err) => console.error(err),
-      { timeout: 30000 }
-    );
+    async function loadDevs() {
+      const { data } = await api.get("/devs");
+      setDevs(data);
+    }
+    loadDevs();
   }, []);
+
+  async function handleAddDev(devInfo) {
+    const { data } = await api.post("/devs", devInfo);
+
+    setDevs([data, ...devs]);
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Register</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Github Username</label>
-            <input
-              type="text"
-              name="github_username"
-              id="github_username"
-              required
-            />
-          </div>
-          <div className="input-block">
-            <label htmlFor="techs">Techs</label>
-            <input type="text" name="techs" id="techs" required />
-          </div>
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input type="text" name="latitude" id="latitude" required />
-            </div>
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input type="text" name="longitude" id="longitude" required />
-            </div>
-          </div>
-          <button type="submit">Save</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars.githubusercontent.com/u/72581482?v=4"
-                alt="Raymw1"
-              />
-              <div className="user-info">
-                <strong>Rayan Wilbert</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              I am looking for challenges that improve simple things in people's
-              routine.
-            </p>
-            <a href="https://github.com/Raymw1">Github Profile</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars.githubusercontent.com/u/72581482?v=4"
-                alt="Raymw1"
-              />
-              <div className="user-info">
-                <strong>Rayan Wilbert</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              I am looking for challenges that improve simple things in people's
-              routine.
-            </p>
-            <a href="https://github.com/Raymw1">Github Profile</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars.githubusercontent.com/u/72581482?v=4"
-                alt="Raymw1"
-              />
-              <div className="user-info">
-                <strong>Rayan Wilbert</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              I am looking for challenges that improve simple things in people's
-              routine.
-            </p>
-            <a href="https://github.com/Raymw1">Github Profile</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars.githubusercontent.com/u/72581482?v=4"
-                alt="Raymw1"
-              />
-              <div className="user-info">
-                <strong>Rayan Wilbert</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              I am looking for challenges that improve simple things in people's
-              routine.
-            </p>
-            <a href="https://github.com/Raymw1">Github Profile</a>
-          </li>
+          {devs.map((dev) => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
