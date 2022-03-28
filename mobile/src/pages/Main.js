72bc587f632234e5d09ce4c7152b344c5dfa0ff9
Raymw1 <rayan.rmw@gmail.com>
2022-03-27 22:46:24 -0300

    Mobile - default main page

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,10 +1,38 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import MapView from "react-native-maps";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
 
 export default function Main() {
-  return (
-    <View>
-      <Text>Main</Text>
-    </View>
-  );
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialLocation() {
+      const { granted } = await requestForegroundPermissionsAsync();
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync();
+        const { latitude, longitude } = coords;
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        });
+      }
+    }
+    loadInitialLocation();
+  }, []);
+
+  if (!currentRegion) return null;
+
+  return <MapView style={styles.map} initialRegion={currentRegion} />;
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+});
